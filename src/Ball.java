8ea28e3f0c0827244fbbb2772e7329da14d090ee
sjdwyer16 <62851540+sjdwyer16@users.noreply.github.com>
2020-04-05 16:13:28 -0500

    All files after working  session on 4.5

--- src/Ball.java
+++ src/Ball.java
@@ -1,4 +1,30 @@
+/**
+ * This class initializes ball for game play
+ * Sets/gets position  
+ * @author jacob. muizz, raheel
+ */
 
 public class Ball {
-
+    
+    /**
+     * This constructor will create the ball for game play
+     * Includes size, color, speed, position to start 
+     */
+    public Ball(){
+        
+    }
+    
+    /**
+     * This method will set position X and position Y for ball location 
+     */
+    public void setPositionBall(int posX, int posY) {
+        
+    }
+    
+    /**
+     * This method will get position X and position Y for ball location
+     */
+    public int[] getPositionBall() {
+        
+    }
 }
--- src/Score.java
+++ src/Score.java
@@ -1,4 +1,31 @@
+/**
+ * This class sets and maintains score 
+ * @author jacob, muizz, raheel
+ */
 
 public class Score {
-
+    
+    private int score;
+    
+    /**
+     * This constructor initalizes the score at 0
+     */
+    public Score() {
+        
+        this.score = 0;
+    }
+    
+    /**
+     * This method updates the score +1 when a brick has been hit once
+     */
+    public void pointScored(){
+        this.score++;
+    }
+    
+    /**
+     * This method gets the current score 
+     */
+    public int getCurrentScore() {
+        return score; 
+    }
 }
