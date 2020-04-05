adbc2a07a5e8d37e20b5c0e3bc3c3a0ee7b2b963
sjdwyer16 <62851540+sjdwyer16@users.noreply.github.com>
2020-04-05 16:07:16 -0500

    updated after 4.5 working session

---
+++ src/Runner.java
@@ -0,0 +1,16 @@
+/**
+ * Runner class for the entire program
+ * @author jacob, muizz, raheel
+ *
+ */
+public class Runner {
+    
+    /**
+     * This method runs the entire program until user decides to exit 
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
