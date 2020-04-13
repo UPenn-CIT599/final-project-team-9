f2aff6d603798eaca44b56ed173d558468573c14
sjdwyer16 <62851540+sjdwyer16@users.noreply.github.com>
2020-04-13 01:31:22 -0500

    updated Main Menu class... pending items are where to put name, how to launch high score file, formatting buttons, add ball

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,3 +1,17 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Desktop;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  * This class initializes main menu
  * Prompts user for difficulty setting and user name  
@@ -6,10 +20,16 @@
  * @author jacob, muizz, raheel 
  *
  */
-public class MainMenu {
+
+public class MainMenu implements ActionListener{
     
-    private String difficulty; 
-    private String username;
+    private JPanel titleNamePanel, easyButtonPanel, medButtonPanel, hardButtonPanel, highscorePanel, exitPanel;
+    private JLabel titleNameLabel;
+    private JButton easyButton, medButton, hardButton, highscoreButton, exitButton;
+    private int x = 400, y = 600;
+    private int velx = 1, vely = 1;
+    private JFrame window = new JFrame();
+    //method for ball display on menu 
     
     /**
      * Initializes display and welcomes the user to the game, prompts difficulty selection 
@@ -18,24 +38,141 @@ public class MainMenu {
      * Displays high scores from High Score class 
      * Closes main menu window 
      */
+    
     public MainMenu() {
         
-    }
-    
-    /**
-     * Exit the game if option is selected 
-     * @return
-     */
-    private String exitListener() {
+        //create the new window 
+        
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.LIGHT_GRAY);
+        window.setLayout(null);
+        window.setTitle("Menu Test");
+        Container container = window.getContentPane();
+        
+        //jpanel for title 
+        //flashing background idea 
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100, 100, 600, 100);
+        titleNamePanel.setBackground(Color.MAGENTA);
+        //titleNamePanel.setFont(Font.SANS_SERIF);
+        
+        //add ball bouncing around in the background
+        
+        
+        
+        //jlabel
+        titleNameLabel = new JLabel("Brick Breaker");
+        titleNameLabel.setForeground(Color.white);
+        titleNameLabel.setFont(new Font("Helvetica", Font.BOLD, 75));
+        
+        
+        //easy button panel
+        easyButtonPanel = new JPanel();
+        easyButtonPanel.setBounds(100, 400, 160, 50);
+        easyButtonPanel.setBackground(Color.green);
+        
+        //easy button
+        easyButton = new JButton("Easy");
+        easyButton.setFont(new Font("Helvetica", Font.PLAIN, 25));
+        easyButton.setBackground(Color.GREEN);
+        easyButton.setForeground(Color.white);
+        easyButton.addActionListener(this);
+        
+        
+        //medium button panel
+        medButtonPanel = new JPanel();
+        medButtonPanel.setBounds(300, 400, 160, 50);
+        medButtonPanel.setBackground(Color.orange);
+        
+        //medium button
+        medButton = new JButton("Medium");
+        medButton.setFont(new Font("Helvetica", Font.PLAIN, 25));
+        medButton.setBackground(Color.orange);
+        medButton.setForeground(Color.white);
+        medButton.addActionListener(this);
+        
+        //hard button panel
+        hardButtonPanel = new JPanel();
+        hardButtonPanel.setBounds(500, 400, 160, 50);
+        hardButtonPanel.setBackground(Color.red);
+        
+        //hard button
+        String SQR = "Hard";
+        hardButton = new JButton(SQR);
+        hardButton.setFont(new Font("Helvetica", Font.PLAIN, 25));
+        hardButton.setBackground(Color.red);
+        hardButton.setForeground(Color.white);
+        hardButton.addActionListener(this);
+        
+        //high score button panel
+        highscorePanel = new JPanel();
+        highscorePanel.setBounds(275, 250, 250, 70);
+        highscorePanel.setBackground(Color.DARK_GRAY);
+        
+        //high score button
+        highscoreButton = new JButton("High Scores");
+        highscoreButton.setFont(new Font("Helvetica", Font.PLAIN, 40));
+        highscoreButton.setBackground(Color.DARK_GRAY);
+        highscoreButton.setForeground(Color.white);
+        highscoreButton.addActionListener(this);
+        
+        //exit button panel
+        exitPanel = new JPanel();
+        exitPanel.setBounds(375, 500, 50, 35);
+        exitPanel.setBackground(Color.LIGHT_GRAY);
+        
+        //medium button
+        exitButton = new JButton("Exit");
+        exitButton.setFont(new Font("Helvetica", Font.ITALIC, 15));
+        exitButton.setBackground(Color.LIGHT_GRAY);
+        exitButton.setForeground(Color.black);
+        exitButton.addActionListener(this);
+        
+        //build panel
+        titleNamePanel.add(titleNameLabel);
+        easyButtonPanel.add(easyButton);
+        medButtonPanel.add(medButton);
+        hardButtonPanel.add(hardButton);
+        highscorePanel.add(highscoreButton);
+        exitPanel.add(exitButton);
+        container.add(titleNamePanel);
+        container.add(highscorePanel);
+        container.add(easyButtonPanel);
+        container.add(medButtonPanel);
+        container.add(hardButtonPanel);
+        container.add(exitPanel);
+        
+        
+        
+        //set visible (keep as last line)
+        window.setVisible(true);
         
     }
     
-    /**
-     * Main constructor calls on the difficulty listener to read the user input
-     * Based on user input, sets difficulty 
-     */
-    private String difficultyListener() {
-        
+
+
+    public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
+        if (action.equals("Easy")) {
+           window.dispose(); 
+           //method to launch new game with easy
+        }
+        else if(action.equals("Medium")) {
+            window.dispose();
+            //method to launch new game with medium
+        }
+        else if(action.equals("Hard")) {
+            window.dispose();
+            //method to launch new game with hard
+        }
+        else if(action.equals("Exit")){
+            window.dispose();
+        }
+        else if(action.equals("High Scores")) {
+            //what do we want the high score button to do??
+        }
+    
     }
     
     /**
@@ -43,6 +180,7 @@ public class MainMenu {
      * @return
      */
     public String getDifficulty() {
+        return null;
         
     }
     
@@ -51,7 +189,16 @@ public class MainMenu {
      * @return
      */
     public String getUserName() {
+        return null;
         
     }
+    
+    public static void main(String[] args){
+        
+      new MainMenu();
+        
+    }
+
+
 
 }
--- src/Paddle.java
+++ src/Paddle.java
@@ -123,9 +123,9 @@ public class Paddle extends JPanel implements ActionListener, KeyListener{
         JFrame jf = new JFrame();
         jf.setTitle("Paddle Test");
         jf.setSize(600, 600);
-        jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.add(p);
+        jf.setVisible(true);
         
     }
     
