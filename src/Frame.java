f0648216c2083016a2617a273bcafe4d1a9f57ad
sjdwyer16 <62851540+sjdwyer16@users.noreply.github.com>
2020-04-18 10:31:07 -0500

    Updating

---
+++ src/Frame.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+
+
+public class Frame extends JFrame{
+    
+    private final int frameheight = 600;
+    private final int framewidth = 600;
+
+    private Panel panel = new Panel();
+    
+    public Frame() {
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(frameheight, framewidth);
+        add(panel);
+        setTitle("Test");
+        setVisible(true);
+        setResizable(false);
+        
+    }
+    
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+    }
+    
+}
