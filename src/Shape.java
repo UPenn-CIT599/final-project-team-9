ee00611f44efee9161b4b5fbd5c571402814392c
sjdwyer16 <62851540+sjdwyer16@users.noreply.github.com>
2020-04-18 10:37:07 -0500

    shape file

--- src/Shape.java
+++ src/Shape.java
@@ -1,4 +1,54 @@
+import java.awt.*;
+import java.awt.geom.*;
+
 
 public class Shape {
+    
+    private Color color;
+    private boolean fill;
+    private RectangularShape shape;
+    
+    public Shape(RectangularShape shape, Color color, boolean fill) {
+        
+        this.shape = shape;
+        this.color = color;
+        this.fill = fill;
+    }
+    
+    public Color getColor(){
+        return color;
+    }
+    
+    public void changeColor(Color color) {
+        this.color = color;
+    }
+    
+    public void draw(Graphics2D g2) {
+        g2.setPaint(color);
+        g2.draw(shape);
+        if(fill) {
+            g2.fill(shape);
+        }
+    }
+    
+    public double getX() {
+        return shape.getX();
+    }
+    
+    public double getY() {
+        return shape.getY();
+    }
+    
+    public double getHeight() {
+        return shape.getHeight();
+    }
+    
+    public double getWidth() {
+        return shape.getWidth();
+    }
+    
+    public void move (int dx, int dy) {
+        shape.setFrame(getX() + dx, getY()+ dy, getWidth(), getHeight());
+    }
 
 }
